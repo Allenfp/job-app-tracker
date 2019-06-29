2c6d16d95bd5fe0fdab06ee06eff79d2acb400e2
Allen <allenfp@gmail.com>
2019-06-29 15:06:48 -0700

    Check Point: Basics Complete.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,7 +14,16 @@ class JobsController < ApplicationController
                 redirect to '/job/new'
             else
                 binding.pry
-                @job = current_user.jobs.create(title: params[:title])
+                @job = current_user.jobs.create(title: params[:title],
+                    job_description: params[:job_description],
+                    min_years_exp: params[:min_years_exp],
+                    website_applied: params[:website_applied],
+                    resume_used: params[:resume_used],
+                    contacted: params[:contacted],
+                    onsite: params[:onsite],
+                    offer: params[:offer],
+                    offer_amount: params[:offer_amount],
+                    user_id: params[:user_id])
                 redirect '/profile'
             end  
         else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
     get '/signup' do
-        binding.pry
         if logged_in?
             redirect to '/users/profile'
         else
@@ -19,14 +18,32 @@ class UsersController < ApplicationController
         end
     end
 
+    post '/login' do
+        user = User.find_by(:username => params[:email])
+        if user && user.authenticate(params[:pwd])
+            session[:user_id] = user.id
+            redirect "/profile"
+        else
+            redirect '/'
+        end
+    end
+
     get '/profile' do 
 
         if logged_in?
             @user = User.find_by_id(session[:user_id])
+            @jobs = @user.jobs.all
+            # binding.pry
             erb :'users/profile'
         else
             redirect to '/'
         end
+    end
+
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 
 end
